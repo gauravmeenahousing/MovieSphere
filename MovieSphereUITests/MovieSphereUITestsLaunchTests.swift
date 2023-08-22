c6c8b555876dda768c08fe2d2648af1b6d72e525
Gaurav Meena <gaurav.meena1@housing.com>
2023-08-22 14:05:25 +0530

    Initial Commit

---
+++ MovieSphereUITests/MovieSphereUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieSphereUITestsLaunchTests.swift
+//  MovieSphereUITests
+//
+//  Created by Gaurav Meena on 22/08/23.
+//
+
+import XCTest
+
+final class MovieSphereUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
